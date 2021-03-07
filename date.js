262d926b12f045c1beb8ff628f3b9926fc19b00f
Saiful Islam <saiful.cse136@gmail.com>
2021-03-07 19:21:35 +0600

    practicing JS Date objects

---
+++ date.js
@@ -0,0 +1,10 @@
+let d=new Date();
+console.log(d);
+
+//The paraameters are specifying year, month, day, hour, minute, second, and millisecond (in order):
+let a = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(a);
+
+let c = new Date(2020,11);
+console.log(c);
+
