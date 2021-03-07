cf505aef795f77f758d6e02df6a149a6c0d37685
Saiful Islam <saiful.cse136@gmail.com>
2021-03-08 01:32:04 +0600

    completed the clock

--- clock.js
+++ clock.js
@@ -2,7 +2,9 @@
 let d =new Date();
 document.write(d);
 
-// location.reload(), 1000;
+// setTimeout(function () { location.reload(1); }, 1000); 
 
-// setTimeout(function () { location.reload(1); }, 1000);
-
+//Make it using arrow function. It will reload the page after 1 second (1000 milliseconds)
+setTimeout( () => {
+    location.reload();    
+}, 1000);
